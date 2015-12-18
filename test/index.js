96648bae71669671be5502ed73042c57289607e5
Contra <contra@maricopa.edu>
2015-12-18 13:27:05 -0800

    init

---
+++ test/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var should = require('should');
+var app-config-chain = require('../');
+
+describe('FUNCTIONNAME()', function() {
+  it('should TASKNAME', function(done) {
+    should.exist(app-config-chain);
+    done();
+  });
+});
